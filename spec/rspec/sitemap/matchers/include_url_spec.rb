5f8b0cf89762ba75a87e7728e122701147e7ccee
Attila Györffy <attila@attilagyorffy.com>
2012-08-29 14:31:51 +0200

    Use `shared_examples_for` and DRY up specs.

--- spec/rspec/sitemap/matchers/include_url_spec.rb
+++ spec/rspec/sitemap/matchers/include_url_spec.rb
@@ -3,8 +3,7 @@ require "spec_helper"
 describe "include_url matcher" do
   include RSpec::Sitemap::Matchers
 
-  context "on a File" do
-    let(:sitemap) { fixture('basic') }
+  shared_examples_for "a matcher that accepts a File or a String" do
     it "passes" do
       sitemap.should include_url('http://www.example.com')
     end
@@ -16,25 +15,14 @@ describe "include_url matcher" do
         e.message.should match("to include a URL to http://www.not-an-example.com")
       }
     end
-
   end
 
-  context "on a String" do
+  it_should_behave_like "a matcher that accepts a File or a String" do
+    let(:sitemap) { fixture('basic') }
+  end
 
+  it_should_behave_like "a matcher that accepts a File or a String" do
     let(:sitemap) { fixture('basic').read }
-
-    it "passes" do
-      sitemap.should include_url('http://www.example.com')
-    end
-
-    it "fails" do
-      expect {
-        sitemap.should include_url('http://www.not-an-example.com')
-      }.to raise_error {|e|
-        e.message.should match("to include a URL to http://www.not-an-example.com")
-      }
-    end
-
   end
 
 end
