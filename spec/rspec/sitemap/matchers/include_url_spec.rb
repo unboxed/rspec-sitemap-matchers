ac58da6a06040422a0f594be525538adab76e58f
Attila Györffy <attila@attilagyorffy.com>
2012-09-02 12:50:49 +0200

    Test IncludeUrl matcher on any object that has a :read method.

--- spec/rspec/sitemap/matchers/include_url_spec.rb
+++ spec/rspec/sitemap/matchers/include_url_spec.rb
@@ -2,27 +2,41 @@ require "spec_helper"
 
 describe "include_url matcher" do
   include RSpec::Sitemap::Matchers
+  
+  describe "#matches?" do
 
-  shared_examples_for "a matcher that accepts a File or a String" do
-    it "passes" do
-      sitemap.should include_url('http://www.example.com')
+    shared_examples_for "a matcher that accepts a File or a String" do
+      it "passes" do
+        sitemap.should include_url('http://www.example.com')
+      end
+
+      it "fails" do
+        expect {
+          sitemap.should include_url('http://www.not-an-example.com')
+        }.to raise_error { |error|
+          error.message.should match("to include a URL to http://www.not-an-example.com")
+        }
+      end
     end
 
-    it "fails" do
-      expect {
-        sitemap.should include_url('http://www.not-an-example.com')
-      }.to raise_error { |error|
-        error.message.should match("to include a URL to http://www.not-an-example.com")
-      }
+    it_should_behave_like "a matcher that accepts a File or a String" do
+      let(:sitemap) { fixture('basic') }
     end
-  end
 
-  it_should_behave_like "a matcher that accepts a File or a String" do
-    let(:sitemap) { fixture('basic') }
-  end
-
-  it_should_behave_like "a matcher that accepts a File or a String" do
-    let(:sitemap) { fixture('basic').read }
+    it_should_behave_like "a matcher that accepts a File or a String" do
+      let(:sitemap) { fixture('basic').read }
+    end
+    
+    context "on any IO object that has a :read method" do
+      let(:sitemap) { mock('sitemap') }
+      before do
+        sitemap.stub(:read).and_return(fixture('basic').read)
+      end
+    
+      it "should accept any IO that has a :read method on it" do
+        sitemap.should include_url('http://www.example.com')
+      end
+    end
   end
 
   describe "#priority" do
